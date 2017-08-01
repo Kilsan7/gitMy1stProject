a9b0bbae4811b8d7dff8d52f3c9922f8f9f8c18a
Kilsan7 <nexus7@naver.com>
2017-08-02 02:58:09 +0900

    .ignore File 생성(gitignore.io 사이트에서 eclipse, visualStudio, androidStudio, itelliJ, Java 입력 - 소스 외 파일들 목록 생성하여 관리에서 제외함)

---
+++ I. SourceCodes/1. C_Sharp Souce Codes/C_Sharp_Perfect_Number/C_Sharp_Perfect_Number/Program.cs
@@ -0,0 +1,68 @@
+﻿/***********************************
+ * 
+ * Sololearn Coding Challenge
+ * Perfect Numbers Logic using C#
+ * Updated : From Int Array -> Dynamic Array, For Loop -> For-each Loop
+ * (2017. 7. 31)
+ *
+ ***********************************/
+using System;
+using System.Collections.Generic;
+
+namespace C_Sharp_PerfectNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, sum, limit;
+            int founded = 0;
+            List<String> factorArr = new List<String>();
+
+            Console.WriteLine("Please input the limit range(ex. 10000).");
+            limit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("I'll show you some perfect numbers!");
+
+            for (i = 1; i <= 40; i++)
+            {
+                Console.Write("*");     // for decoration
+            }
+            Console.WriteLine();
+
+            for (i = 2; i <= limit; i++)
+            {
+                sum = 0;
+                factorArr.Clear();
+                for (n = 1; n <= (i / 2); n++)
+                {
+                    if (i % n == 0)
+                    {
+                        sum += n;
+                        factorArr.Add(Convert.ToString(n));
+                    }
+                }
+                if (sum == i)
+                {
+                    founded++;    // For counting the number of founded prefect numbers
+
+                    Console.Write("{0} = ", i);
+                    foreach (var Item in factorArr)
+                    {
+                        Console.Write(Item);
+                        int k = factorArr.IndexOf(Item);
+
+                        if (k != (factorArr.Count - 1))
+                            Console.Write(" + ");
+                        else
+                            Console.WriteLine();     // New line at the end of one array
+                    }
+                }
+            }
+            for (i = 1; i <= 40; i++)
+            {
+                Console.Write("*");   // for decoration
+            }
+            Console.WriteLine("\nPerfect numbers founded in the range is : {0}", founded);
+        }
+    }
+}
